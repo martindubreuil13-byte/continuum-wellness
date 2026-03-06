939708eeb529bf520a89947917a2383118952b1d
Martin Dubreuil <martindubreuil13@gmail.com>
2026-03-06 14:20:50 +0700

    Fix next config for production build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    enabled: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
